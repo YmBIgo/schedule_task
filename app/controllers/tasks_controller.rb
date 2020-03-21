0a2584b580667f9145bb12bb6181b0dc40dcb0d7
YmBIgo <IgYmB0218LP@gmail.com>
2020-03-21 20:11:59 +0900

    fix basic design

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
     def show
         @task = Task.find(params[:id])
+        if @task.parent_task_id != 0 then
+            @parent_task = Task.find_by(:parent_task_id => @task.parent_task_id)
+        end
+        @relate_tasks = Task.where(:t_number => @task.t_number, :work_id => @task.work_id).where.not(:id => @task.id)
     end
     def index
     end
