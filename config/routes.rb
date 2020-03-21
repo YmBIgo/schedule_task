52535bfbe9485fef3b0e62a51d3e6b02f9dd4f06
YmBIgo <IgYmB0218LP@gmail.com>
2020-03-21 18:23:52 +0900

    impl edit&add&delete at registeration page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/top', to: "top#index"
-  root 'top#index'
+  root to: 'top#index'
   get '/top_second', to: "static_page#index"
   resources :works, :only => [:new, :show, :index, :create]
   resources :tasks, :only => [:show]
